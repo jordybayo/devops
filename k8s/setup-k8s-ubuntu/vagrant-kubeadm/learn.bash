c9b4cef4cbf9946b369358bb197503de65e497d1
youngdevps <jordiibayo@gmail.com>
2022-06-03 12:15:22 +0100

    kubectl administration command

---
+++ k8s/setup-k8s-ubuntu/vagrant-kubeadm/learn.bash
@@ -0,0 +1,24 @@
+# add a USER and grant it sudo privileges
+adduser student 
+usermod -aG wheel student
+# get existing pods 
+kubectl get pods
+# get existing deployments
+kubectl get deployments
+# know what i can do with a specific user
+kubectl auth can-i create deployments --as user
+# will show api grous ressources
+kubectl api-resources | less
+# will show api version
+kubectl api-version 
+# can be use to explore api components
+kubectl explain
+# enable bashc completion in kubectl
+kubectl completion bash
+kubectl completion bash >> ~/.bashrc #to make it persistent
+# will show help for kubectl
+kubectl -h | less
+# enable bashc completion in kubectl for everybody
+sudo -i
+cd /etc/bash_completion.d
+kubectl completion bash > /etc/bash_completion.d/kubectl
